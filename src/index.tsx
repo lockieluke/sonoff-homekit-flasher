f2163ad8e877e6c55064613fefdc8c62e5717b68
Sherlock Luk <sherlockluk@outlook.com>
2025-06-03 20:25:25 +0100

    Remove sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@
 import { render } from "solid-js/web";
 
 import App from "./App";
-import "./index.scss";
+import "./index.css";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
